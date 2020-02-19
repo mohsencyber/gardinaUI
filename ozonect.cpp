76283bf8087e08a85bcc91c675047f4515ac06b1
farhadi <farhadi@gmail.com>
2020-02-19 20:05:44 +0330

    [enhancement]add new form

--- ozonect.cpp
+++ ozonect.cpp
@@ -6,9 +6,65 @@ OzoneCT::OzoneCT(QWidget *parent) :
     ui(new Ui::OzoneCT)
 {
     ui->setupUi(this);
+    this->setStyleSheet( " #OzoneCT { "
+                         " border:none;border-image: url(:/gardina_main_bg.png) 0 0 0 0 stretch stretch;"
+                         "}");
+    ui->pauseButton->setDisabled(true);
+    isPause = true;
 }
 
 OzoneCT::~OzoneCT()
 {
     delete ui;
+}
+
+void OzoneCT::on_pushButton_clicked()
+{
+    close();
+}
+
+void OzoneCT::on_startButton_clicked()
+{
+    ui->startButton->setDisabled(true);
+    ui->pushButton->setDisabled(true);
+    ui->pauseButton->setDisabled(false);
+}
+
+void OzoneCT::on_pauseButton_clicked()
+{
+    if ( isPause )
+    {
+        ui->pauseButton->setText("Continue        ");
+        ui->pauseButton->setStyleSheet("QPushButton#pauseButton:hover{"
+                                    "border:none;"
+                                    "border-image: url(:/button_actions_continue_highlight_bg.png);"
+                                    "}"
+                                    ""
+                                    "QPushButton#pauseButton{"
+                                    "border:none;"
+                                    "border-image: url(:/button_actions_continue_bg.png);"
+                                    "}");
+        isPause = false;
+    }else
+    {
+        ui->pauseButton->setText("Pause           ");
+        ui->pauseButton->setStyleSheet("QPushButton#pauseButton:hover{"
+                                    "border:none;"
+                                    "border-image: url(:/button_actions_pause_highlight_bg.png);"
+                                    "}"
+                                    ""
+                                    "QPushButton#pauseButton{"
+                                    "border:none;"
+                                    "border-image: url(:/button_actions_pause_bg.png);"
+                                    "}");
+        isPause = true;
+    }
+
+}
+
+void OzoneCT::on_stopButton_clicked()
+{
+    ui->startButton->setDisabled(false);
+    ui->pushButton->setDisabled(false);
+    ui->pauseButton->setDisabled(true);
 }
--- ozonect.h
+++ ozonect.h
@@ -15,8 +15,18 @@ public:
     explicit OzoneCT(QWidget *parent = nullptr);
     ~OzoneCT();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_startButton_clicked();
+
+    void on_pauseButton_clicked();
+
+    void on_stopButton_clicked();
+
 private:
     Ui::OzoneCT *ui;
+    bool isPause;
 };
 
 #endif // OZONECT_H
